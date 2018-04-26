7e29b14fd6bc4390b2117647f4e4ab04a9688adb
杨雄 <xiong.yang@dianrong.com>
2018-04-26 14:26:11 +0800

    添加测试类

---
+++ myPodName/Classes/YXTest.h
@@ -0,0 +1,12 @@
+//
+//  YXTest.h
+//  myPodName
+//
+//  Created by 杨雄 on 2018/4/26.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXTest : NSObject
+
+@end
---
+++ myPodName/Classes/YXTest.m
@@ -0,0 +1,14 @@
+//
+//  YXTest.m
+//  myPodName
+//
+//  Created by 杨雄 on 2018/4/26.
+//
+
+#import "YXTest.h"
+
+@implementation YXTest
+
+
+
+@end
